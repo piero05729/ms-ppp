50968fa3d9811bd4b80a2ea115c364f4cebe4979
PIERO\LENOVO <Andres>
2025-04-21 09:44:16 -0500

    actualizado

--- src/main/java/upeu/edu/pe/msppp/repository/PppRepository.java
+++ src/main/java/upeu/edu/pe/msppp/repository/PppRepository.java
@@ -4,10 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import upeu.edu.pe.msppp.domain.Ppp;
 
-@Repository // Marca la interfaz como un repositorio de Spring
+@Repository
 public interface PppRepository extends JpaRepository<Ppp, Long> {
-
-    // Cuenta las prácticas de un estudiante que tienen un estado específico (por ejemplo, rechazadas)
+    // Cuenta las prácticas rechazadas del estudiante
     long countByEstudianteIdAndEstado(Long estudianteId, String estado);
 
     // Verifica si el estudiante ya tiene una práctica en la misma empresa
